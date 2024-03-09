11d5f5975c685c3035f9b30973fa4eccb477e0e8
NagaEswar379 <154249319+NagaEswar379@users.noreply.github.com>
2024-03-10 00:39:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,99 @@
+import joblib
+import os
+from flask import Flask, render_template, request
+from flask_caching import Cache
+from flask_wtf.csrf import CSRFProtect
+
+app = Flask(__name__)  # Creating Flask application instance
+csrf = CSRFProtect(app)  # Adding CSRF protection to the app
+# Configure Flask-Caching to use on-disk caching
+cache_dir = os.path.join(app.root_path, '.cache')
+cache = Cache(app, config={'CACHE_TYPE': 'filesystem', 'CACHE_DIR': cache_dir})
+# Loading the trained models
+# Loading text-based phishing detection model
+text_model = joblib.load(r"C:\Users\venka\Downloads\PhishShield-testing\PhishShield-testing\models\feature_model.joblib")
+# Loading feature-based phishing detection model
+feature_model = joblib.load(r"C:\Users\venka\Downloads\PhishShield-testing\PhishShield-testing\models\feature_model.joblib")
+
+
+@app.route('/')  # Defining route for the home page
+def home():
+    """
+    Renders the home page.
+
+    Returns:
+        rendered HTML template
+    """
+    return render_template('index.html')  # Rendering the HTML template for the home page
+
+
+# Defining route for phishing detection
+@app.route('/detect', methods=['POST'])
+@csrf.exempt  # Exempting CSRF protection for this route
+def detect_phishing():
+    """
+    Detects phishing URLs.
+
+    Returns:
+        rendered HTML template with the prediction result
+    """
+    if request.method == 'POST':
+        url = request.form["url"]  # Getting URL from the form data
+        # Check if the result is cached
+        cached_result = cache.get(url)
+        if cached_result:
+            return cached_result
+
+        # Making prediction using feature-based model
+        feature_pred = feature_model.predict([url])
+        print(feature_pred[0])
+
+        # Making prediction using text-based model
+        text_pred = text_model.predict([url])
+        print(text_pred[0])
+
+        # Choosing final prediction based on confidence scores
+        if feature_pred == text_pred:
+            final_pred = feature_pred
+        else:
+            # Getting confidence scores from feature-based model
+            feature_confidence = feature_model.predict_proba([url])
+            print(feature_confidence)
+            # Getting confidence scores from text-based model
+            text_confidence = text_model.predict_proba([url])
+            print(text_confidence)
+            # Extracting confidence scores for phishing and legitimate classes
+            confidence_phishing_feature = feature_confidence[:,
+                                                             0] if feature_confidence.shape[1] > 1 else 1 - feature_confidence[:, 0]
+            print(confidence_phishing_feature)
+            confidence_legitimate_feature = feature_confidence[:,
+                                                               1] if feature_confidence.shape[1] > 1 else feature_confidence[:, 0]
+            print(confidence_legitimate_feature)
+            confidence_phishing_text = text_confidence[:,
+                                                       0] if text_confidence.shape[1] > 1 else 1 - text_confidence[:, 0]
+            print(confidence_phishing_text)
+            confidence_legitimate_text = text_confidence[:,
+                                                         1] if text_confidence.shape[1] > 1 else text_confidence[:, 0]
+            print(confidence_legitimate_text)
+            # Defining weights for each model's confidence score
+            weight_feature = 0.6
+            weight_text = 0.4
+
+            # Combining confidence scores using weighted average7i
+            combined_confidence_phishing = (
+                weight_feature * confidence_phishing_feature) + (weight_text * confidence_phishing_text)
+            combined_confidence_legitimate = (
+                weight_feature * confidence_legitimate_feature) + (weight_text * confidence_legitimate_text)
+
+            final_pred = -1 if combined_confidence_phishing > combined_confidence_legitimate else 1
+
+        # Converting prediction to human-readable format
+        result = "Phishing" if final_pred == -1 else "Legitimate"
+        cache.set(url, render_template('index.html', url=url, result=result),
+                  timeout=3600)  # Cache for 1 hour (3600 seconds)
+        # Returning prediction and confidence scores to be rendered in the HTML template
+        return render_template('index.html', url=url, result=result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Running the Flask application in debug mode
---
+++ build_text_model.py
@@ -0,0 +1,144 @@
+import pandas as pd
+from sklearn import metrics
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer
+from preprocessing.preprocess_url import URLPreprocessor
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from sklearn import metrics
+import joblib
+
+# Load the dataset
+# import nltk
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+
+def load_data(file_path):
+    df = pd.read_csv(file_path)
+    df = df.sample(frac=1).reset_index(drop=True)  # Shuffle the dataset
+    X = df['url']
+    y = df['status']
+    return X, y
+
+# Split the dataset into training and testing sets
+
+
+def split_data(X, y, test_size=0.2, random_state=42):
+    return train_test_split(X, y, test_size=test_size, random_state=random_state)
+
+# Define parameter grids for GridSearchCV
+param_grids = {
+    "Logistic Regression": {'solver': ['liblinear'], 'penalty': ['l1', 'l2'], 'C': [0.01, 0.1, 1.0, 10, 20]},
+    "LinearSVC": {'C': [0.01, 0.1, 1.0, 10, 20], 'penalty': ['l2'], 'loss': ['hinge', 'squared_hinge']},
+    "Random Forest": {'n_estimators': [50, 100, 200], 'max_depth': [None, 10, 20], 'min_samples_split': [2, 5, 10]},
+    "K-Nearest Neighbors": {'n_neighbors': [3, 5, 10], 'weights': ['uniform', 'distance'], 'metric': ['minkowski'], 'p': [1, 2]},
+    "Multinomial Naive Bayes": {'alpha': [0.1, 1.0, 10]}
+}
+
+# Function to train and evaluate a model
+
+
+def train_evaluate_model(pipeline, X_train, X_test, y_train, y_test):
+    pipeline.fit(X_train, y_train)
+    y_train_pred = pipeline.predict(X_train)
+    y_test_pred = pipeline.predict(X_test)
+    acc_train = metrics.accuracy_score(y_train, y_train_pred)
+    acc_test = metrics.accuracy_score(y_test, y_test_pred)
+    f1_train = metrics.f1_score(y_train, y_train_pred)
+    f1_test = metrics.f1_score(y_test, y_test_pred)
+    recall_train = metrics.recall_score(y_train, y_train_pred)
+    recall_test = metrics.recall_score(y_test, y_test_pred)
+    precision_train = metrics.precision_score(y_train, y_train_pred)
+    precision_test = metrics.precision_score(y_test, y_test_pred)
+    return acc_train, acc_test, f1_train, f1_test, recall_train, recall_test, precision_train, precision_test
+
+
+def store_results(ML_Model, accuracy_train, accuracy_test, f1_score_train, f1_score_test, recall_train, recall_test, precision_train, precision_test):
+    results = {"ML Model": ML_Model, "Accuracy (Train)": accuracy_train, "Accuracy (Test)": accuracy_test, "F1 Score (Train)": f1_score_train, "F1 Score (Test)": f1_score_test, "Recall (Train)": recall_train, "Recall (Test)": recall_test, "Precision (Train)": precision_train, "Precision (Test)": precision_test}
+    return results
+
+def main():
+    # Load data
+    X, y = load_data(r'C:\Users\venka\Downloads\PhishShield-testing\PhishShield-testing\datafiles\dataset_for_text_model.csv')
+
+    # Split data
+    X_train, X_test, y_train, y_test = split_data(X, y)
+
+    # Define pipeline for each classifier
+    classifiers = {
+        "Logistic Regression": Pipeline([
+            ('preprocessor', URLPreprocessor()),
+            ('vectorizer', CountVectorizer(tokenizer=None,
+             stop_words=None, lowercase=False, ngram_range=(1, 2))),
+            ('classifier', GridSearchCV(LogisticRegression(),
+             param_grids["Logistic Regression"], cv=5, scoring='f1'))
+        ])
+     #   "LinearSVC": Pipeline([
+      #      ('preprocessor', URLPreprocessor()),
+      #      ('vectorizer', CountVectorizer(tokenizer=None,
+      #       stop_words=None, lowercase=False, ngram_range=(1, 2))),
+      #      ('classifier', GridSearchCV(LinearSVC(),
+       #      param_grids["LinearSVC"], cv=5, scoring='f1'))
+      #  ]),
+      #  "Random Forest": Pipeline([
+      #      ('preprocessor', URLPreprocessor()),
+      #      ('vectorizer', CountVectorizer(tokenizer=None,
+      #       stop_words=None, lowercase=False, ngram_range=(1, 2))),
+      #      ('classifier', GridSearchCV(RandomForestClassifier(),
+      #       param_grids["Random Forest"], cv=5, scoring='f1'))
+       # ]),
+       # "K-Nearest Neighbors": Pipeline([
+       #     ('preprocessor', URLPreprocessor()),
+       #     ('vectorizer', CountVectorizer(tokenizer=None,
+       #      stop_words=None, lowercase=False, ngram_range=(1, 2))),
+       #     ('classifier', GridSearchCV(KNeighborsClassifier(),
+       #      param_grids["K-Nearest Neighbors"], cv=5, scoring='f1'))
+       # ]),
+       # "Multinomial Naive Bayes": Pipeline([
+       #     ('preprocessor', URLPreprocessor()),
+       #     ('vectorizer', CountVectorizer(tokenizer=None,
+       #      stop_words=None, lowercase=False, ngram_range=(1, 2))),
+       #     ('classifier', GridSearchCV(MultinomialNB(),
+       #      param_grids["Multinomial Naive Bayes"], cv=5, scoring='f1'))
+       # ])
+    }
+
+    best_model = None
+    best_accuracy = 0
+    best_model_name = ""
+
+    # Train and evaluate models
+    results = []
+    for name, model in classifiers.items():
+        acc_train, acc_test, f1_train, f1_test, recall_train, recall_test, precision_train, precision_test = train_evaluate_model(
+            model, X_train, X_test, y_train, y_test)
+        if acc_test > best_accuracy:
+            best_accuracy = acc_test
+            best_model = model
+            best_model_name = name
+        results.append(store_results(name, acc_train, acc_test, f1_train, f1_test, recall_train, recall_test, precision_train, precision_test))
+
+        # Generate classification report
+        y_test_pred = model.predict(X_test)
+        print(f"Classification Report for {name}:")
+        print(metrics.classification_report(y_test, y_test_pred, target_names=('Phishing', 'Legitimate')))
+
+
+    # Save the best model
+    if best_model is not None:
+        joblib.dump(best_model, r'C:\Users\venka\Downloads\PhishShield-testing\PhishShield-testing\models\text_model.joblib')
+        print(f"Best performing model saved as 'text_model.joblib'. Classifier: {best_model_name}, Accuracy: {best_accuracy}")
+
+    # Display results
+    results_df = pd.DataFrame(results)
+    sorted_results = results_df.sort_values(
+        by=['Accuracy (Test)', 'F1 Score (Test)'], ascending=False).reset_index(drop=True)
+    print(sorted_results)
+
+
+if __name__ == "__main__":
+    main()
